8653f5ff8888df4fe7ee72e5d75db5f03c89ab58
Bohdan <vinn.andrew@gmail.com>
2020-11-14 22:09:54 +0200

    feat: Add ipmort path to the API_PATH

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://uxm7hab1p4.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://agdgfiw0a6.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
